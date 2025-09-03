df33f8296cc1df1cb18dac83f35e47dd897b0cde
choyoonseo12 <choyoonseo1@icloud.com>
2025-09-03 11:53:56 +0900

    회원가입 처리 완료

--- src/main/java/com/my/securityTest/config/SecurityConfig.java
+++ src/main/java/com/my/securityTest/config/SecurityConfig.java
@@ -5,30 +5,37 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    // 비밀번호 암초 처리 기계 추가
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     // controller로 가지 않고, Security로 간다.
     // 여러개의 Filter
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/", "/login", "/join", "/joinProc").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 );
-                // 로그인 요청 처리
-        http.formLogin((auth)->auth
+        // 로그인 요청 처리
+        http.formLogin((auth) -> auth
                 .loginPage("/login")
                 .loginProcessingUrl("/loginProc")
                 .permitAll()
         );
         http
-                .csrf((auth)->auth.disable());
-            return http.build();
+                .csrf((auth) -> auth.disable());
+        return http.build();
     }
 }
